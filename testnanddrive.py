95dddf26f8b3030f2f7bd642f91cbcae4cb7df7d
AndreoBotelho <45693863+AndreoBotelho@users.noreply.github.com>
2024-05-07 04:11:06 -0300

    Update testnanddrive.py

--- testnanddrive.py
+++ testnanddrive.py
@@ -1,3 +1,26 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Andre Botelho
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 import memdrive
 import shutil
 import os
@@ -10,12 +33,12 @@ src = open(sourcename,'r')
 samples = 1000
 
 for i in range(samples):
-    try:
-        os.remove(destname.format(i))
-    except:
-        pass
-#     dst = open(destname.format(i+600),'w')
-#     shutil.copyfileobj(src,dst)
-#     src.seek(0)
-#     dst.close()
+    #try:
+   #     os.remove(destname.format(i))
+   # except:
+   #     pass
+    dst = open(destname.format(i+600),'w')
+    shutil.copyfileobj(src,dst)
+    src.seek(0)
+    dst.close()
     
